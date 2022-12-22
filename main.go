97cc812faa81f41951fc9f8fd15a73d5b5bc48c2
duck8823 <duck8823@gmail.com>
2022-12-22 13:18:25 +0900

    S1038: should use log.Printf(...) instead of log.Println(fmt.Sprintf(...))

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 //go:generate openapi-generator generate -g go -i openapi.yml -o generated/go/user --package-name user --global-property=models,apis,supportingFiles=configuration.go:client.go:utils.go,apiTests=false,apiDocs=false,modelTests=false,modelDocs=false
 func HandleRequest(ctx context.Context, user user.User) (string, error) {
-	log.Println(fmt.Sprintf("%#v", user))
+	log.Printf("%#v\n", user)
 	return fmt.Sprintf("Hello %s!", user.Name), nil
 }
 
